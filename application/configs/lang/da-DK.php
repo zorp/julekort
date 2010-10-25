34288658a6ac3c8a6d69de7fa2862a2a429c0295
Christian Kirkegaard <christian@art-box.dk>
2010-10-25 15:11:49 +0200

    basic setup stuff

---
+++ application/configs/lang/da-DK.php
@@ -0,0 +1,25 @@
+<?php
+
+return array(
+    /*Zend_Validate_Alnum::STRING_EMPTY              => '',
+    Zend_Validate_Alpha::NOT_ALPHA                 => '',
+    Zend_Validate_Alpha::STRING_EMPTY              => '',
+    Zend_Validate_Between::NOT_BETWEEN             => '',
+    Zend_Validate_Between::NOT_BETWEEN_STRICT      => '',
+    Zend_Validate_Date::INVALID                    => '',
+    Zend_Validate_Digits::NOT_DIGITS               => '',
+    Zend_Validate_Digits::STRING_EMPTY             => '',
+    Zend_Validate_EmailAddress::INVALID            => '',
+    Zend_Validate_EmailAddress::INVALID_HOSTNAME   => '',
+    Zend_Validate_EmailAddress::INVALID_MX_RECORD  => '',
+    Zend_Validate_EmailAddress::DOT_ATOM           => '',
+    Zend_Validate_EmailAddress::QUOTED_STRING      => '',
+    Zend_Validate_EmailAddress::INVALID_LOCAL_PART => '',
+    Zend_Validate_Float::NOT_FLOAT                 => '',
+    Zend_Validate_InArray::NOT_IN_ARRAY            => '',
+    Zend_Validate_Int::NOT_INT                     => '',
+    Zend_Validate_LessThan::NOT_LESS               => '',*/
+    Zend_Validate_NotEmpty::IS_EMPTY               => 'Dette felt er krævet',
+    /*Zend_Validate_StringLength::TOO_SHORT          => '',
+    Zend_Validate_StringLength::TOO_LONG           => '',*/
+);
