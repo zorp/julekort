e92a822a2d6fa4ba6a321dff9490299ee74f62dd
Christian Kirkegaard <christian@art-box.dk>
2010-10-28 16:12:06 +0200

    added body class helper

---
+++ library/Verk/View/Helper/BodyClass.php
@@ -0,0 +1,23 @@
+<?php
+
+class Verk_View_Helper_BodyClass extends Zend_View_Helper_Abstract {
+
+    protected $_data = array();
+
+    public function BodyClass($return = null)
+    {
+        if(is_array($return)) {
+            foreach($return as $key) {
+                $request = Zend_Controller_Front::getInstance()->getRequest();
+                $params = $request->getParams();
+
+                if(array_key_exists($key, $params)) {
+                    array_push($this->_data, $params[$key]);
+                }
+            }
+        }
+
+        return implode(' ', $this->_data);
+    }
+
+}
